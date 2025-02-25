70051ca7a8c7e2684da01bdf4c7dabb3af0b4811
Racha, Suresh <suresh.racha@optum.com>
2025-02-25 15:20:18 +0530

    changes

---
+++ package/wrappers-lib/testContext.ts
@@ -0,0 +1,13 @@
+
+import { Logger } from "winston";
+
+const testCotext = {
+    // @ts-ignore 
+    logger: undefined as Logger,
+    runtimeStorageFile: undefined as string,
+    assertsJson: undefined as any,
+    runtimeLoggerFile: undefined as string,
+    appUserId: undefined as string,
+    appUserPwd: undefined as string
+}
+export default testCotext;
