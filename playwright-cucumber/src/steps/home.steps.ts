7a24eb8bdbeedaecb9c5ec343864ca92446cca42
sureshracha <31437248+sureshracha@users.noreply.github.com>
2025-03-14 15:32:13 +0530

    changes

--- playwright-cucumber/src/steps/home.steps.ts
+++ playwright-cucumber/src/steps/home.steps.ts
@@ -1,6 +1,6 @@
-import { Then } from '@cucumber/cucumber';
+import { Then, setDefaultTimeout } from '@cucumber/cucumber';
 import { HomePage } from '../pages/home.po';
-
+setDefaultTimeout(160000);
 Then('Check home page displayed', async () => {
-    await new HomePage().checkHomePageExisted(); 
+    await new HomePage().checkHomePageExisted();
 });
--- playwright-cucumber/src/steps/login.steps.ts
+++ playwright-cucumber/src/steps/login.steps.ts
@@ -1,7 +1,7 @@
-import { Then } from '@cucumber/cucumber';
+import { Then, setDefaultTimeout } from '@cucumber/cucumber';
 import { LoginPage } from '../pages/login.po';
 const loginData = require('../testdata/ref/loginDetails.json');
-
+setDefaultTimeout(160000);
 Then('Launch and login to application', async () => {
     const loginPage = new LoginPage();
     await loginPage.launch("https://www.saucedemo.com/");
